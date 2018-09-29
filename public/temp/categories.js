79d2acb62a5cb052b67ac333c1e891c67a6f0759
Richard Prasad <richarddprasad@gmail.com>
2018-09-28 19:37:52 -0700

    Attempt 2

--- public/temp/categories.js
+++ public/temp/categories.js
@@ -1,19 +1,18 @@
-export const CATEGORIES = {
-    "categories": [
-        {"name": "age21", "image": "https://i.postimg.cc/85CwM96P/a21.png"},
-        {"name": "after5pm", "image": "https://i.postimg.cc/RFbdRd71/after5pm.png"},
-        {"name": "art", "image": "https://i.postimg.cc/WbrRdqVY/art.png"},
-        {"name": "casino", "image": "https://i.postimg.cc/668xN1mX/casinotiles.png"},
-        {"name": "charity", "image": "https://i.postimg.cc/DfRkfd9b/charity.png"},
-        {"name": "comedy", "image": "https://i.postimg.cc/g2qfD9nv/comedytiles.png"},
-        {"name": "concerts", "image": "https://i.postimg.cc/yxMPqNrk/concerts.png"},
-        {"name": "conventions", "image": "https://i.postimg.cc/7hGVQz9F/conventions.png"},
-        {"name": "dance", "image": "https://i.postimg.cc/x1SwMjTf/dance.png"},
-        {"name": "environmental", "image": "https://i.postimg.cc/pT77TR6t/environmental.png"},
-        {"name": "farmersmarket", "image": "https://i.postimg.cc/cJF2pQ7C/farmmarket.png"},
-        {"name": "festivals", "image": "https://i.postimg.cc/4dnqsmw5/festivals.png"},
-        {"name": "foodtruck", "image": "https://i.postimg.cc/1XqdV2kV/foodtruck.png"},
-        {"name": "movies", "image": "https://i.postimg.cc/KvtVGX7d/movies.png"},
-        {"name": "premium", "image": "https://i.postimg.cc/0yfBSpBn/premium.png"},
-        {"name": "sports", "image": "https://i.postimg.cc/76CsjjYt/sports.png"}
-]};
+export const CATEGORIES = [
+    { "name": "age21", "image": "https://i.postimg.cc/85CwM96P/a21.png" },
+    { "name": "after5pm", "image": "https://i.postimg.cc/RFbdRd71/after5pm.png" },
+    { "name": "art", "image": "https://i.postimg.cc/WbrRdqVY/art.png" },
+    { "name": "casino", "image": "https://i.postimg.cc/668xN1mX/casinotiles.png" },
+    { "name": "charity", "image": "https://i.postimg.cc/DfRkfd9b/charity.png" },
+    { "name": "comedy", "image": "https://i.postimg.cc/g2qfD9nv/comedytiles.png" },
+    { "name": "concerts", "image": "https://i.postimg.cc/yxMPqNrk/concerts.png" },
+    { "name": "conventions", "image": "https://i.postimg.cc/7hGVQz9F/conventions.png" },
+    { "name": "dance", "image": "https://i.postimg.cc/x1SwMjTf/dance.png" },
+    { "name": "environmental", "image": "https://i.postimg.cc/pT77TR6t/environmental.png" },
+    { "name": "farmersmarket", "image": "https://i.postimg.cc/cJF2pQ7C/farmmarket.png" },
+    { "name": "festivals", "image": "https://i.postimg.cc/4dnqsmw5/festivals.png" },
+    { "name": "foodtruck", "image": "https://i.postimg.cc/1XqdV2kV/foodtruck.png" },
+    { "name": "movies", "image": "https://i.postimg.cc/KvtVGX7d/movies.png" },
+    { "name": "premium", "image": "https://i.postimg.cc/0yfBSpBn/premium.png" },
+    { "name": "sports", "image": "https://i.postimg.cc/76CsjjYt/sports.png" }
+];
